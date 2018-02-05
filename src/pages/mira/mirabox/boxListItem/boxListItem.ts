f4310ed4ff94e982dd5d35ded7e12d199d5b159f
emonogarov <emonogarov@gmail.com>
2018-02-05 07:29:32 +0300

    MiraBox Simple nominal viewer
    Mirabox simple nominal viewer

--- src/pages/mira/mirabox/boxListItem/boxListItem.ts
+++ src/pages/mira/mirabox/boxListItem/boxListItem.ts
@@ -1,15 +1,31 @@
 import {Component, Input} from '@angular/core';
-import {MiraBox} from "../../../../mira/mira";
+import {MiraBox, MiraBoxType} from "../../../../mira/mira";
+import {NavController} from "ionic-angular";
+import {NominalBoxViewer} from "../miraboxViewer/nominalBoxViewer/nominalBoxViewer";
 
 @Component({
   selector: 'mirabox-list-item',
   templateUrl: 'boxListItem.html'
 })
 export class MiraBoxListItem {
+  constructor(private navCtrl: NavController) {
+  }
+
   @Input()
   public miraBox: MiraBox;
 
   public goToMiraBoxView(): void {
     console.log(this.miraBox.getGuid());
+    switch (this.miraBox.getType()) {
+      case MiraBoxType.Nominal:
+        this.navCtrl.push(NominalBoxViewer, this.miraBox);
+        break;
+      case MiraBoxType.Multi:
+      case MiraBoxType.Smart:
+        console.info('Not implemented yet');
+        break;
+      default:
+        throw 'fucked up';
+    }
   }
 }
