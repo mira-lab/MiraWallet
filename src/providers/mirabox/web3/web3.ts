4088628d399e3aaffb59dd4c4db6066e8d3a4056
Daniil <34211974+daniilmp@users.noreply.github.com>
2018-03-15 21:19:34 +0300

    Add get templates func; add MiraFactory abi.

--- src/providers/mirabox/web3/web3.ts
+++ src/providers/mirabox/web3/web3.ts
@@ -2,31 +2,40 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Web3Provider {
+  private Web3;
+  private web3;
+  constructor(nodeURL:string){
+    this.Web3 = require('web3');
+    this.web3 = new this.Web3(new this.Web3.providers.HttpProvider('http://localhost:8545'));
+  }
   public checkConnection(network: string) {
-    var Web3 = this.getWeb3();
-    const web3 = new Web3(new Web3.providers.HttpProvider(network));
-    web3.eth.net.isListening()
+    this.web3.eth.net.isListening()
       .then(() => console.log('Connected.'))
       .catch(e => console.log('Something went wrong:' + e));
   }
 
   public getWeb3() {
-    return require('web3');
+    return this.Web3;
   }
 
   public checkMiraBoxOpened(hash: string) {
-    //tododaniil Change node and contract addresses to live network
-    let Web3 = require('web3');
-    let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
     let permissionContractAddress = '0x731a10897d267e19B34503aD902d0A29173Ba4B1';
-    //change abi if contract changes
     let permissionContractAbi = require("./abiPermissioning.json");
-
-    let permissionContract = new web3.eth.Contract(permissionContractAbi, permissionContractAddress);
-
+    let permissionContract = new this.web3.eth.Contract(permissionContractAbi, permissionContractAddress);
     return new Promise((resolve) => {
       permissionContract.methods.getMiraBoxOpened(hash)
+        .call()
+        .then((result) => {
+          resolve(result)
+        });
+    });
+  }
+  private getTemplates(_miraFactoryAddress: string){
+    let miraFactoryAddress = _miraFactoryAddress;
+    let miraFactoryAbi = require("./MiraFactory.json");
+    let miraFactoryContract = new this.web3.eth.Contract(miraFactoryAbi, miraFactoryAddress);
+    return new Promise((resolve) => {
+      miraFactoryContract.methods.getTemplatesList()
         .call()
         .then((result) => {
           resolve(result)
